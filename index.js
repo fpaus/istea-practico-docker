cfffde07a0cc4fb64aac198810cb74307bea57a9
Fabrizio Dante Pauselli <fabriziopauselli@gmail.com>
2025-06-09 17:38:06 -0300

    Refactor output method and adjust formatting in index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import {stdin, stdout} from "node:process";
-import { createInterface } from "node:readline/promises";
-
+import { stdin, stdout } from 'node:process';
+import { createInterface } from 'node:readline/promises';
 
 const rl = createInterface({
-	input: stdin,
-	output: stdout
+  input: stdin,
+  output: stdout,
 });
 
 const answer = await rl.question("What's your name? ");
 
-console.log(`Hello, ${answer}`);
+stdout.write(`Hello, ${answer}!\n`);
 
 rl.close();
